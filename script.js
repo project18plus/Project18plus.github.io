4a229e2cbcca1a690c2954e6540e0987c7dd60d5
project18plus <project279270@gmail.com>
2026-04-16 16:58:13 +0700

    script.js

--- script.js
+++ script.js
@@ -1,44 +1,52 @@
-// =========================
-// 1. IKLAN BIASA (1 LINK)
-// =========================
-// Cara pakai di HTML:
-// onclick="openAd(TEST_AD_1)"
-function openAd(https://www.profitablecpmratenetwork.com/gdp3y55e1?key=276987812d5d44414d3c88174a146b38) {
-  window.open(url, '_blank');
+body{
+margin:0;
+font-family:Inter;
+background:#ffffff;
 }
 
-
-// =========================
-// 2. RANDOM ADS (ACAK)
-// =========================
-// Cara pakai di HTML:
-// onclick="openRandomAd()"
-function openRandomAd() {
-  const ads = [TEST_AD_1, TEST_AD_2, TEST_AD_3];
-
-  const random = ads[Math.floor(Math.random() * ads.length)];
-  window.open(random, '_blank');
+.appbar{
+position:sticky;
+top:0;
+background:white;
+padding:15px;
+border-bottom:1px solid #eee;
 }
 
-
-// =========================
-// 3. IKLAN DENGAN DELAY
-// =========================
-// Cara pakai:
-// onclick="openAdDelay(TEST_AD_1)"
-function openAdDelay(url) {
-  setTimeout(() => {
-    window.open(url, '_blank');
-  }, 2000); // 2 detik
+.brand{
+font-weight:400;
+display:flex;
+gap:10px;
+align-items:center;
 }
 
+.logo{
+background:white;
+color:black;
+padding:5px 10px;
+border-radius:6px;
+}
 
-// =========================
-// 4. ADS KOMBINASI (DOUBLE ACTION)
-// =========================
-// Cara pakai:
-// onclick="openWithRedirect(TEST_AD_1, 'player.html')"
-function openWithRedirect(adUrl, fakePage) {
-  window.open(adUrl, '_blank');
-  window.location.href = fakePage;
+.wrap{
+padding:20px;
+display:flex;
+flex-direction:column;
+align-items:center;
+}
+
+.phone{
+width:350px;
+background:white;
+border-radius:20px;
+overflow:hidden;
+}
+
+video{
+width:100%;
+display:block;
+}
+
+.meta{
+margin-top:15px;
+color:#666;
+font-size:14px;
 }
